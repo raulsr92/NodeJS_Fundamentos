b17f7f5c699067429698fadbce34e41421cbeadb
Raul Sanchez R. <raul.sr92@gmail.com>
2025-04-22 09:25:26 -0500

    Clase 4: tipos de modulos en Node JS - modulos de terceros con npm

---
+++ src/third-party.js
@@ -0,0 +1,8 @@
+
+const isOdd = require('is-odd');
+
+
+console.log(`4 is Odd?: ${isOdd(4)} `)
+console.log(`7 is Odd?: ${isOdd(7)} `)
+console.log(`12 is Odd?: ${isOdd(12)} `)
+console.log(`139 is Odd?: ${isOdd(139)} `)
